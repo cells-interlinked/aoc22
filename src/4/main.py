f1ac386db37991389b8ab4c4e92a291e843d6831
cells-interlinked <kody.fields@outlook.com>
2022-12-06 10:21:31 -0500

    Completed day4 - part2

--- src/4/main.py
+++ src/4/main.py
@@ -17,6 +17,25 @@ def parse_pair(pair):
 
     return False
 
+def parse_pair_two(pair):
+    sections = pair.split(',')
+    section1 = sections[0].split('-')
+    section2 = sections[1].split('-')
+
+    section1_start = int(section1[0])
+    section1_end = int(section1[1])
+
+    section2_start = int(section2[0])
+    section2_end = int(section2[1])
+
+    if (section1_start <= section2_start and section1_end >= section2_start 
+        or section1_start <= section2_end and section1_end >= section2_end
+        or section2_start <= section1_start and section2_end >= section1_start
+        or section2_start <= section1_end and section2_end >= section1_end):
+        return True
+
+    return False
+
 def part_one():
     input_file = open('input.txt', 'r')
     lines = input_file.readlines()
@@ -31,4 +50,19 @@ def part_one():
 
     print('part one: ' + str(contained_pairs))
 
+def part_two():
+    input_file = open('input.txt', 'r')
+    lines = input_file.readlines()
+
+    contained_pairs = 0
+
+    for line in lines:
+        pair_contained = parse_pair_two(line)
+
+        if (pair_contained):
+            contained_pairs += 1
+
+    print('part two: ' + str(contained_pairs))
+
 part_one()
+part_two()
