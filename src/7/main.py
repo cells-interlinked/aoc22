0981d82679a11a63194068bf3f25826e8c87c0b4
cells-interlinked <kody.fields@outlook.com>
2022-12-07 23:34:23 -0500

    Completed day7 - part1

---
+++ src/7/main.py
@@ -0,0 +1,79 @@
+
+class Node:
+    def __init__(self, name, size, parent):
+        self.name = name
+        self.size = size
+        self.parent = parent
+        self.children = []
+    running_total = 0
+    
+    def get_child(self, name):
+        for i in range(len(self.children)):
+            if self.children[i].name == name:
+                return self.children[i]
+
+        return None
+
+    def print(self, depth):
+        print((depth * '    - ') + self.name + ' (' + str(self.size) + ')')
+
+        for child in self.children:
+            child.print(depth+1)
+
+    def get_total_size(self, nodes_to_delete):
+        total_size = 0
+        for child in self.children:
+            if child.size > 0:
+                total_size += child.size
+            else:
+                total_size += child.get_total_size(nodes_to_delete)
+        
+        if (total_size <= 100000): nodes_to_delete.append(total_size)
+        return total_size
+
+def part_one():
+    input_file = open('input.txt', 'r')
+    lines = input_file.readlines()
+
+    root_node = Node('/', 0, None)
+    current_node = root_node
+
+    for line in lines:
+        if line.strip() == '$ cd /':
+            current_node = root_node
+        elif line.strip() == '$ cd ..':
+            if current_node.parent != None:
+                current_node = current_node.parent
+        elif line.strip().startswith('$ cd'):
+            directory_name = line.strip().split(' ')[2]
+            target_child = current_node.get_child(directory_name)
+            if target_child != None:
+                current_node = target_child
+            else:
+                new_node = Node(directory_name, 0, current_node)
+                current_node.children.append(new_node)
+                current_node = new_node
+        elif line.strip() == '$ ls':
+            continue
+        elif line.startswith('dir'):
+            directory_name = line.strip().split(' ')[1]
+            target_child = current_node.get_child(directory_name)
+
+            if target_child == None:
+                new_node = Node(directory_name, 0, current_node)
+                current_node.children.append(new_node)
+        else:
+            file_size = line.strip().split(' ')[0]
+            file_name = line.strip().split(' ')[1]
+            target_child = current_node.get_child(file_name)
+            if target_child == None:
+                new_node = Node(file_name, int(file_size), current_node)
+                current_node.children.append(new_node)
+
+    nodes_to_delete = []
+    root_node.print(0)
+    root_node.get_total_size(nodes_to_delete)
+
+    print('part one: ' + str(sum(nodes_to_delete)))
+
+part_one()
