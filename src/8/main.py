cd7cd0dc9a423c9b9010b59e3ce027419fc89837
cells-interlinked <kody.fields@outlook.com>
2022-12-08 19:42:33 -0500

    Completed day8 - part2

--- src/8/main.py
+++ src/8/main.py
@@ -69,4 +69,76 @@ def part_one():
 
     print('part one: ' + str(visible_cells))
 
-part_one()
+def determine_scenic(x, y, grid, width, height):
+    cell_height = grid[y][x]
+    
+    left = 0
+    right = 0
+    up = 0
+    down = 0
+
+    view_distance = 0
+    #right
+    for i in range(x+1, width):
+        view_distance += 1
+        target_height = grid[y][i]
+        if target_height >= cell_height:
+            break
+    right = view_distance
+    view_distance = 0
+    #left
+    for i in range(x-1, -1, -1):
+        view_distance += 1
+        target_height = grid[y][i]
+        if target_height >= cell_height:
+            break
+    left = view_distance
+    view_distance = 0
+    #up
+    for i in range(y-1, -1, -1):
+        view_distance += 1
+        target_height = grid[i][x]
+        if target_height >= cell_height:
+            break
+    up = view_distance
+    view_distance = 0
+    #down
+    for i in range(y+1, height):
+        view_distance += 1
+        target_height = grid[i][x]
+        if target_height >= cell_height:
+            break
+    down = view_distance
+    return left * right * up * down
+
+def part_two():
+    input_file = open('Z:\\dev\\aoc22\\src\\8\\input.txt', 'r')
+    lines = input_file.readlines()
+
+    width = len(lines[0].strip())
+    height = len(lines)
+    print('width: ' + str(width) + ' height: ' + str(height))
+
+    top_score = 0
+
+    grid = []
+
+    for i in range(height):
+        grid.append([])
+        for j in range(width):
+            grid[i].append(int(lines[i].strip()[j]))
+
+    for i in range(height):
+        row = ''
+        for j in range(width):
+            scenic_score = determine_scenic(j, i, grid, width, height)
+            row += str(scenic_score)
+            if scenic_score > top_score:
+                top_score = scenic_score
+                
+        print(row)
+
+    print('part two: ' + str(top_score))
+
+#part_one()
+part_two()
